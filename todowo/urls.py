9fc362f37e8f369862300ddbe576e2ac6044dfeb
unknown <varunsam9900@gmail.com>
2020-12-19 13:35:03 +0530

    first commit

---
+++ todowo/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #Auth
+    path('signupuser', views.signupuser, name='signupuser'),
+    path('logoutuser', views.logoutuser, name='logoutuser'),
+    path('loginuser', views.loginuser, name='loginuser'),
+
+    #todos
+    path('current', views.currenttodos, name='currenttodos'),
+    path('completed', views.completedtodos, name='completedtodos'),
+    path('', views.home, name='home'),
+    path('create', views.createtodo, name='createtodo'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
+]
